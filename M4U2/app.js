ac001ddaac6a16cb03236c3a9eb8550a86a27556
GuidoSeverio <guido.severio@homtail.com>
2023-07-22 12:20:02 -0300

    m4u2

--- M4U2/app.js
+++ M4U2/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+var pool = require('./models/bd.js');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +24,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//select
+//pool.query("select * from empleados"). then(function(resultados){
+//  console.log(resultados);
+//})
+
+//insert
+
+//var obj = {
+//  nombre: 'Adrian',
+//  apellido: 'Perez'
+//}
+
+//pool.query("insert into empleados set ?", [obj]).then(function(resultados){
+//  console.log(resultados)
+//});
+
+//uptdate
+
+//var id = 24
+//var obj = {
+//  nombre: 'Pablo',
+//  apellido: 'Gomez'
+//}
+
+//pool.query("update empleados set ? where id_emp=?", [obj, id]).then(function(resultados){
+//  console.log(resultados);
+//})
+
+//delete
+
+
+var id = 1;
+
+pool.query("delete from empleados where id_emp = ?", [id]).then(function(resultados){
+  console.log(resultados);
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
